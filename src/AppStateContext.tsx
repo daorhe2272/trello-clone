018aa27c960ff41c13cc41956908fe8d6fb99d76
David Orozco <daorhe2272@hotmail.com>
2021-08-14 12:19:28 +0200

    First commit

---
+++ src/AppStateContext.tsx
@@ -0,0 +1,116 @@
+import React, { createContext, useContext, useReducer } from "react";
+import { v4 } from "uuid";
+
+import { findItemIndexById } from "./utils/findItemIndexById";
+import { moveItem } from "./moveItem";
+import { DragItem } from "./DragItem";
+
+interface Task {
+  id: string
+  text: string
+}
+
+interface List {
+  id: string
+  text: string
+  tasks: Task[]
+}
+
+export interface AppState {
+  draggedItem: DragItem | undefined;
+  lists: List[]
+}
+
+const appData: AppState = {
+  draggedItem: undefined,
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "c0", text: "Generate app scaffold" }]
+    },
+    {
+      id: "1",
+      text: "In Progress",
+      tasks: [{ id: "c2", text: "Learn TypeScript"}]
+    },
+    {
+      id: "2",
+      text: "Done",
+      tasks: [{ id: "c3", text: "Begin to use static typing"}]
+    }
+  ]
+}
+
+interface AppStateContextProps {
+  state: AppState;
+  dispatch: React.Dispatch<any>;
+}
+
+const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps);
+
+export const useAppState = () => {
+  return useContext(AppStateContext);
+}
+
+export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
+  const [state, dispatch] = useReducer(appStateReducer, appData);
+
+  return (
+    <AppStateContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppStateContext.Provider>
+  )
+}
+
+type Action =
+  { type: "ADD_LIST"; payload: string; } |
+  { type: "ADD_TASK"; payload: { text: string; taskId: string; } } |
+  { type: "MOVE_LIST"; payload: { dragIndex: number; hoverIndex: number; } } |
+  { type: "SET_DRAGGED_ITEM"; payload: DragItem | undefined; }
+
+const appStateReducer = (state: AppState, action: Action): AppState => {
+  switch (action.type) {
+    case "ADD_LIST": {
+      return {
+        ...state,
+        lists: [
+          ...state.lists,
+          { id: v4(), text: action.payload, tasks: [] }
+        ]
+      }
+    }
+
+    case "ADD_TASK": {
+      const targetLaneIndex = findItemIndexById(
+        state.lists,
+        action.payload.taskId
+      )
+      state.lists[targetLaneIndex].tasks.push({
+        id: v4(),
+        text: action.payload.text
+      })
+
+      return {
+        ...state
+      }
+    }
+
+    case "MOVE_LIST": {
+      const { dragIndex, hoverIndex } = action.payload;
+      state.lists = moveItem(state.lists, dragIndex, hoverIndex);
+
+      return {
+        ...state
+      }
+    }
+
+    case "SET_DRAGGED_ITEM": {
+      return { ...state, draggedItem: action.payload }
+    }
+    
+    default: {
+      return state;
+    }
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { DndProvider } from "react-dnd";
+import Backend from "react-dnd-html5-backend";
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AppStateProvider } from "./AppStateContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <DndProvider backend={Backend}>
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
+  </DndProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
